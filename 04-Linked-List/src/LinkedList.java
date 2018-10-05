769125a8d8d903820a23be6eba067ac7ae027b78
mcrwayfun <mcrwayfun@gmail.com>
2018-10-06 01:49:39 +0800

    finish 链表

--- 04-Linked-List/src/LinkedList.java
+++ 04-Linked-List/src/LinkedList.java
@@ -54,7 +54,7 @@ public class LinkedList<E> {
     public void add(int index, E e) {
 
         if (index < 0 || index > size)
-            throw new IllegalArgumentException("Cannot add.index is invalid !");
+            throw new IllegalArgumentException("Can not add.index is invalid !");
 
         // find the node before index
         Node prev = dummyHead;
@@ -73,5 +73,134 @@ public class LinkedList<E> {
     // 在链表末尾添加元素e
     public void addLast(E e) {
         add(size, e);
+    }
+
+    // 获得链表的第index(0-based)个位置的元素
+    public E get(int index) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Can not get.index is invalid !");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+
+        return cur.e;
+    }
+
+    // 获得链表的第一个元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    // 获得链表的最后一个元素
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    // 修改链表的第index(0-based)个位置的元素为e
+    public void set(int index, E e) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Can not update.index is invalid !");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    // 查找链表中是否有元素e
+    public boolean contains(E e) {
+
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    // 从链表中删除index(0-based)位置的元素, 返回删除的元素
+    public E remove(int index) {
+
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Can not remove.index is invalid !");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+
+        // the node to remove
+        Node retNode = prev.next;
+        prev.next = prev.next.next;
+        retNode.next = null;
+        size--;
+
+        return retNode.e;
+    }
+
+    // 从链表中删除第一个元素, 返回删除的元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    // 从链表中删除最后一个元素, 返回删除的元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从链表中删除元素e
+    public void removeElement(E e){
+
+        Node prev = dummyHead;
+        while(prev.next != null){
+            if(prev.next.e.equals(e))
+                break;
+            prev = prev.next;
+        }
+
+        if(prev.next != null){
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            size --;
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+
+        Node cur = dummyHead.next;
+        while(cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i = 0 ; i < 5 ; i ++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
     }
 }
