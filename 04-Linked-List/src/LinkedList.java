780265290b4aca561cc70a7da0e52d61cf1fd0c8
mcrwayfun <mcrwayfun@gmail.com>
2018-10-06 01:04:04 +0800

    update 添加dummyHead，修改add方法

--- 04-Linked-List/src/LinkedList.java
+++ 04-Linked-List/src/LinkedList.java
@@ -30,13 +30,13 @@ public class LinkedList<E> {
         }
     }
 
-    // 头结点
-    private Node head;
+    // 虚拟结点
+    private Node dummyHead;
     private int size;
 
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node();
         size = 0;
     }
 
@@ -50,31 +50,26 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    // 在链表头添加元素
-    public void addFirst(E e) {
-
-        head = new Node(e, head);
-        size++;
-    }
-
     // 在链表的index(0-based)位置添加新的元素e
     public void add(int index, E e) {
 
         if (index < 0 || index > size)
             throw new IllegalArgumentException("Cannot add.index is invalid !");
 
-        // index is 0
-        if (index == 0)
-            addFirst(e);
         // find the node before index
-        Node prev = head;
-        for (int i = 0; i < index - 1; i++)
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
             prev = prev.next;
 
         prev.next = new Node(e, prev.next);
         size++;
     }
-        
+
+    // 在链表头添加元素
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
     // 在链表末尾添加元素e
     public void addLast(E e) {
         add(size, e);
