787f8176a22a95ad4d0e93f1e34c4c1a939dc779
mcrwayfun <mcrwayfun@gamil.com>
2018-10-18 16:02:33 +0800

    对使用heapify和不使用heapify方法构造堆进行性能分析

---
+++ 11-Heap/src/Compare.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+/**
+ * @author mcrwayfun
+ * @version 1.0
+ * @description 比较使用heapify和不使用heapify构造堆的性能
+ *              heapify：从最后一个节点的父节点处开始进行siftDown操作
+ * @date Created in 2018/10/18
+ */
+public class Compare {
+
+    private static double testHeap(Integer[] testData, boolean isHeapify){
+
+        long startTime = System.nanoTime();
+
+        MaxHeap<Integer> maxHeap;
+        if(isHeapify)
+            maxHeap = new MaxHeap<>(testData);
+        else{
+            maxHeap = new MaxHeap<>();
+            for(int num: testData)
+                maxHeap.add(num);
+        }
+
+        int[] arr = new int[testData.length];
+        for(int i = 0 ; i < testData.length ; i ++)
+            arr[i] = maxHeap.extractMax();
+
+        for(int i = 1 ; i < testData.length ; i ++)
+            if(arr[i-1] < arr[i])
+                throw new IllegalArgumentException("Error");
+        System.out.println("Test MaxHeap completed.");
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 1000000;
+
+        Random random = new Random();
+        Integer[] testData = new Integer[n];
+        for(int i = 0 ; i < n ; i ++)
+            testData[i] = random.nextInt(Integer.MAX_VALUE);
+
+        double time1 = testHeap(testData, false);
+        System.out.println("Without heapify: " + time1 + " s");
+
+        double time2 = testHeap(testData, true);
+        System.out.println("With heapify: " + time2 + " s");
+    }
+}
