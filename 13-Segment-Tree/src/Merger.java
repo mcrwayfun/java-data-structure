f292af14756cec4090d99db63803078d903cac37
mcrwayfun <mcrwayfun@gamil.com>
2018-10-19 17:29:09 +0800

    线段树的基本实现

---
+++ 13-Segment-Tree/src/Merger.java
@@ -0,0 +1,9 @@
+/**
+ * @author mcrwayfun
+ * @version 1.0
+ * @description
+ * @date Created in 2018/10/19
+ */
+public interface Merger<E> {
+    E merge(E a, E b);
+}
---
+++ 13-Segment-Tree/src/SegmentTree.java
@@ -0,0 +1,134 @@
+/**
+ * @author mcrwayfun
+ * @version 1.0
+ * @description
+ * @date Created in 2018/10/19
+ */
+public class SegmentTree<E> {
+
+    private E[] tree;
+
+    private E[] data;
+
+    private Merger<E> merger;
+
+    @SuppressWarnings("unchecked")
+    public SegmentTree(E[] arr, Merger<E> merger) {
+
+        this.merger = merger;
+
+        data = (E[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++)
+            data[i] = arr[i];
+
+        tree = (E[]) new Object[arr.length * 4];
+        buildSegmentTree(0, 0, arr.length - 1);
+    }
+
+    // 在treeIndex的位置创建表示区间[l...r]的线段树
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        // 仅存在一个子树
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        // 防止溢出
+        int mid = l + (r - l) / 2;
+
+        // 递归左右建树
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    // 返回当前树中的元素个数
+    public int getSize() {
+        return data.length;
+    }
+
+    // 返回index对应的元素
+    public E get(int index) {
+        if (index < 0 || index > 0)
+            throw new IllegalArgumentException("Index is illegal");
+
+        return data[index];
+    }
+
+    // 返回区间[queryL,queryR]的值
+    public E query(int queryL, int queryR) {
+
+        if (queryL < 0 || queryL >= data.length ||
+                queryR < 0 || queryR >= data.length || queryL > queryR)
+            throw new IllegalArgumentException("Index is illegal.");
+
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    // 在以treeIndex为根的线段树中[l...r]的范围里，搜索区间[queryL...queryR]的值
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+
+        if (l == queryL && r == queryR)
+            return tree[treeIndex];
+
+        int mid = l + (r - l) / 2;
+        // treeIndex的节点分为[l...mid]和[mid+1...r]两部分
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        // [queryL...queryR]在[mid+1,r]内
+        if (queryL >= mid + 1)
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+            // [queryL...queryR]在[l,mid]内
+        else if (queryR <= mid)
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+
+        // [queryL...queryR]一半在左区间，一半在右区间
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+
+        return merger.merge(leftResult, rightResult);
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append('[');
+        for (int i = 0; i < tree.length; i++) {
+            if (tree[i] != null)
+                res.append(tree[i]);
+            else
+                res.append("null");
+
+            if (i != tree.length - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+
+        SegmentTree<Integer> segTree = new SegmentTree<>(nums,
+                (a, b) -> a + b);
+        System.out.println(segTree);
+
+        System.out.println(segTree.query(0, 2));
+        System.out.println(segTree.query(2, 5));
+        System.out.println(segTree.query(0, 5));
+    }
+}
