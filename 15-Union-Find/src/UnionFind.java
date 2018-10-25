b334b95f92f7248f644e79e20afb32fef3907da7
mcrwayfun <mcrwayfun@gmail.com>
2018-10-25 23:51:20 +0800

    并集代码编写

--- 15-Union-Find/src/UnionFind.java
+++ 15-Union-Find/src/UnionFind.java
@@ -4,7 +4,7 @@
  * @description
  * @date Created in 2018/10/25
  */
-public class UnionFind {
+public class UnionFind implements UF {
 
 
     // rank[i]表示以i为根的集合所表示的树的层数
@@ -13,7 +13,64 @@ public class UnionFind {
     private int[] rank;
     private int[] parent; // parent[i]表示第i个元素所指向的父节点
 
-    public UnionFind(){
-    	
+    public UnionFind(int size) {
+
+        rank = new int[size];
+        parent = new int[size];
+
+        // parent指向自己，表示每个元素自己成一个集合
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+
+    }
+
+    // 查找过程, 查找元素p所对应的集合编号
+    // O(h)复杂度, h为树的高度
+    private int find(int p) {
+        if (p < 0 || p >= parent.length)
+            throw new IllegalArgumentException("p is out of the range");
+
+        // 压缩路径，递归将所有子树root节点
+        if(p != parent[p])
+            parent[p] = find(parent[p]);
+
+        return parent[p];
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    @Override
+    public boolean isConnected( int p , int q ){
+        return find(p) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    @Override
+    public void unionElements(int p, int q){
+
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if( pRoot == qRoot )
+            return;
+
+        // 根据两个元素所在树的rank不同判断合并方向
+        // 将rank低的集合合并到rank高的集合上
+        if( rank[pRoot] < rank[qRoot] )
+            parent[pRoot] = qRoot;
+        else if( rank[qRoot] < rank[pRoot])
+            parent[qRoot] = pRoot;
+        else{ // rank[pRoot] == rank[qRoot]
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;   // 此时, 维护rank的值
+        }
     }
 }
