9d77860f723c92124896078d34c67ce792bd837e
mcrwayfun <mcrwayfun@gmail.com>
2018-10-26 09:13:01 +0800

    修改注释

--- 15-Union-Find/src/UnionFind.java
+++ 15-Union-Find/src/UnionFind.java
@@ -8,7 +8,7 @@ public class UnionFind implements UF {
 
 
     // rank[i]表示以i为根的集合所表示的树的层数
-    // 在后续的代码中, 我们并不会维护rank的语意, 也就是rank的值在路径压缩的过程中, 有可能不再是树的层数值
+    // rank的值在路径压缩的过程中, 有可能不再是树的层数值
     // 这也是我们的rank不叫height或者depth的原因, 它只是作为比较的一个标准
     private int[] rank;
     private int[] parent; // parent[i]表示第i个元素所指向的父节点
